e5fc371c14c42f055045e5d9ab9181fc88700761
ninjadev999 <radikkaviev83@yandex.ru>
2019-11-15 09:27:54 +0000

    used environements

--- flask-app/hello.py
+++ flask-app/hello.py
@@ -4,8 +4,9 @@ from flask_pymongo import PyMongo
 from encode import JSONEncoder
 
 app = Flask(__name__)
-app.config["MONGO_URI"] =  "mongodb://root:password@mongodb:27017/flaskdb?authSource=admin"
-#app.config["MONGO_URI"] = "mongodb://" + os.environ["MONGODB_USERNAME"] + ":" + os.environ["MONGODB_PASSWORD"] + "@" + os.environ["MONGODB_HOSTNAME"] + ":27017/" + os.environ["MONGODB_DATABASE"]
+#app.config["MONGO_URI"] =  "mongodb://root:password@mongodb:27017/flaskdb?authSource=admin"
+app.config["MONGO_URI"] = "mongodb://" + os.environ["MONGODB_USERNAME"] + ":" + os.environ["MONGODB_PASSWORD"] + "@" + os.environ["MONGODB_HOSTNAME"] + ":27017/" + os.environ["MONGODB_DATABASE"] + "?" + "authSource=admin"
+
 mongo = PyMongo(app)
 db = mongo.db
 
