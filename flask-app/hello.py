aeb65b4f803a7eabf432bbb705c8f1cd5ef13ff0
ninjadev999 <ninjadev999@gmail.com>
2019-11-15 01:13:43 -0800

    fixed mongodb connection issue

--- flask-app/hello.py
+++ flask-app/hello.py
@@ -1,10 +1,11 @@
 import os
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
+from encode import JSONEncoder
 
 app = Flask(__name__)
-app.config["MONGO_URI"] =  "mongodb://mongodb:27017/flaskdb"
-#app.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
+app.config["MONGO_URI"] =  "mongodb://root:password@mongodb:27017/flaskdb?authSource=admin"
+#app.config["MONGO_URI"] = "mongodb://" + os.environ["MONGODB_USERNAME"] + ":" + os.environ["MONGODB_PASSWORD"] + "@" + os.environ["MONGODB_HOSTNAME"] + ":27017/" + os.environ["MONGODB_DATABASE"]
 mongo = PyMongo(app)
 db = mongo.db
 
@@ -12,16 +13,17 @@ db = mongo.db
 def hello():
     return "<h1>Falsk: Hello World!</h1>"
 
-@app.route('/users', methods=['GET'])
+@app.route("/users", methods=["GET"])
 def users():
-    if request.method == 'GET':
-        users = db.user.find()
-        print('==== users: ', users)
+    if request.method == "GET":
+        users = db.users.find()
+        print("==== users: ", users)
         data = []
         for user in users:
-          data.append(user)
-        return jsonify(data), 200
+            user.pop("_id")
+            data.append(user)
+        return JSONEncoder().encode(data), 200
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
