d294c5c872929c68cc06655192ea4c3b11f5bfc2
ninjadev999 <ninjadev999@gmail.com>
2019-11-14 17:37:32 -0800

    fixed angular docker issue

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Home from './Home/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route path="/react-app"  name="Home" render={props => <Home {...props}/>} />
+        <Redirect from="/" to="/react-app"/>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ react-app/src/Home/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './home.css';
+
+export default function Home(){
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <p>
+          Edit <code>src/Home.js</code> and save to reload.
+        </p>
+        <a
+          className="Home-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
